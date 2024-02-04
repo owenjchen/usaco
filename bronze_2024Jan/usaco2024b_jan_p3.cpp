5aae4ec4cb7921de05a3da30e1b2a1f111db7d29
Owen Chen <owenjchen@gmail.com>
2024-02-04 16:42:44 -0500

    fix a typo

--- bronze_2024Jan/usaco2024b_jan_p3.cpp
+++ bronze_2024Jan/usaco2024b_jan_p3.cpp
@@ -7,14 +7,14 @@
 
     Dr. Chen's analysis:
     Perform two accumulated sums:
-        accum_sum1(i) = sum(-bateria(0 ... i))
-        accum_sum2(i) = sum(accum_sum1(0, ..., i) - bateria(i))
-        bateria(i) += accum_sum2(i)
-        res = sum(abs(bateria(i)))
+        accum_sum1(i) = sum(-bacteria(0 ... i))
+        accum_sum2(i) = sum(accum_sum1(0, ..., i) - bacteria(i))
+        bacteria(i) += accum_sum2(i)
+        res = sum(abs(bacteria(i)))
 
     Complexity:
         O(n)
-        
+
     https://github.com/owenjchen/usaco/blob/main/bronze_2024Jan/usaco2024b_jan_p3.cpp    
 */
 #include <bits/stdc++.h>
@@ -32,9 +32,9 @@ int main() {
     ll res = 0, accum_sum1 = 0, accum_sum2 = 0;
     for (int i = 0; i < N; i++)
     {
-        bacteria[i] += accum_sum2; // bacteria[i] += sum(sum(-bateria(0 ... i-1) - bateria(i-1))
-        accum_sum1 -= bacteria[i]; // accum_sum1 = sum(-bateria(0 ... i))
-        accum_sum2 += accum_sum1 - bacteria[i]; // accum_sum2 = sum(sum(-bateria(0 ... i) - bateria(i))
+        bacteria[i] += accum_sum2; // bacteria[i] += sum(sum(-bacteria(0 ... i-1) - bacteria(i-1))
+        accum_sum1 -= bacteria[i]; // accum_sum1 = sum(-bacteria(0 ... i))
+        accum_sum2 += accum_sum1 - bacteria[i]; // accum_sum2 = sum(sum(-bacteria(0 ... i) - bacteria(i))
         res += abs(bacteria[i]);
     }
     cout << res << endl;
