6a697a8a74c6ba8f4b6e6899473bbfe66cddd7d2
Owen Chen <owenjchen@gmail.com>
2024-02-04 16:44:49 -0500

    formating

--- bronze_2024Jan/usaco2024b_jan_p3.cpp
+++ bronze_2024Jan/usaco2024b_jan_p3.cpp
@@ -25,13 +25,11 @@ int main() {
     int N;
     cin >> N;
     vector<ll> bacteria(N);
-    for (int i = 0; i < N; i++)
-    {
+    for (int i = 0; i < N; i++){
         cin >> bacteria[i];
     }
     ll res = 0, accum_sum1 = 0, accum_sum2 = 0;
-    for (int i = 0; i < N; i++)
-    {
+    for (int i = 0; i < N; i++){
         bacteria[i] += accum_sum2; // bacteria[i] += sum(sum(-bacteria(0 ... i-1) - bacteria(i-1))
         accum_sum1 -= bacteria[i]; // accum_sum1 = sum(-bacteria(0 ... i))
         accum_sum2 += accum_sum1 - bacteria[i]; // accum_sum2 = sum(sum(-bacteria(0 ... i) - bacteria(i))
